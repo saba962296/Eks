84022344893e6877857561344abd498a933e01ad
saba962296 <155954718+saba962296@users.noreply.github.com>
2024-12-30 17:02:15 +0530

    Create iam_role_eks_cluster.tf

---
+++ iam_role_eks_cluster.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+
+  tags = {
+    Name = "eks-cluster-role"
+  }
+}
+
+data "aws_iam_policy_document" "eks_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
